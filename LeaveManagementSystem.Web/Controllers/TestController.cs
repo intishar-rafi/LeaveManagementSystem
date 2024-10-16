3c25096f059452b4bc127cb951fe6e9462943d50
smart computer <smart computer@DESKTOP-BKEVJVO>
2024-10-16 20:38:49 +0600

    Add project files.

---
+++ LeaveManagementSystem.Web/Controllers/TestController.cs
@@ -0,0 +1,19 @@
+﻿using LeaveManagementSystem.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LeaveManagementSystem.Web.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            var data = new TestViewModel
+            {
+                Name = "Student",
+                DateOfBirth = new DateTime(2000,10,07)
+                
+            };
+            return View(data);
+        }
+    }
+}
